262c3e399b4f2f7ca36f452eb8138cc41feaf5b2
Luisa Frugoli <luisafrugoli11@gmail.com>
2023-10-07 18:29:58 -0300

    Short introduction to exceptions

--- OOP/television.py
+++ OOP/television.py
@@ -1,20 +1,34 @@
 class Television:
-    def __init__(self): # Constructor
+    def __init__(self, min, cur, max): # Constructor
         self.on=False
-        self.channel=5
         self.size=25
         self.brand="Panasonic"
+        self.cmin = min
+        self.channel = cur
+        self.cmax = max
 
     def change_channels(self): # Class method
         a = input("Would you like to go up or down? U/D ")
         if a.upper() == "U":
-            self.channel+=1
-            print("You are now on channel {}!".format(self.channel))
+            if self.channel + 1 <= self.cmax:
+                self.channel +=1
+                print("You are now on channel {}!".format(self.channel))
+            else:
+                raise ValueError("Reached maximum channel!") # Treating exceptions
         elif a.upper() == "D":
-            self.channel-=1
-            print("You are now on channel {}!".format(self.channel))
+            if self.channel - 1 >= self.cmin:
+                self.channel-=1
+                print("You are now on channel {}!".format(self.channel))
+            else:
+                raise ValueError("Reached minimum channel!")
         else:
             print("Invalid command!")
 
-tv = Television()
-tv.change_channels()
+tv = Television(0, 5, 100)
+
+while True:
+    try:
+        tv.change_channels()
+    except ValueError as e:
+        print(e)
+        break
