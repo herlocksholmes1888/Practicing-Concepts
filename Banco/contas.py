25871e687701c8ce07d0b824795c23c0f0257261
Luisa Frugoli <luisafrugoli11@gmail.com>
2023-10-20 18:04:28 -0300

    Aprendendo sobre herança

--- Banco/contas.py
+++ Banco/contas.py
@@ -20,10 +20,28 @@ class Conta:
         if self.saldo >= valor:
             self.saldo -= valor
             self.operacoes.append(["SAQUE", valor])
+            return True
         else:
             print("O valor que deseja sacar é inferior ao valor que se encontra em sua conta bancária.")
+            return False
         
     def deposito(self, valor):
         self.saldo += valor
         self.operacoes.append(["DEPÓSITO", valor])
+
+class ContaEspecial(Conta):
+    def __init__(self, clientes, numero, saldo, limite = 0):
+        Conta.__init__(self, clientes, numero, saldo)
+        self.limite = limite
+
+    def saque(self, valor):
+        if self.saldo + self.limite >= valor:
+            self.saldo -= valor 
+            self.operacoes.append(["SAQUE: ", valor])
+            return True
+        else:
+            return False
         
+    def extrato(self):
+        Conta.extrato(self)
+        print("Limite atual: %10.2f" % self.limite)
