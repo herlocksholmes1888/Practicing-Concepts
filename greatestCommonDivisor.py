97fbd70559ac9fb6c77a8e9ca8400147aebf7326
Luisa Frugoli <luisafrugoli11@gmail.com>
2024-03-05 13:28:56 -0300

    Finding the GCD through the Euclidean Algorithm

--- greatestCommonDivisor.py
+++ greatestCommonDivisor.py
@@ -3,9 +3,19 @@ def checkIsDigit(i):
         raise ValueError("You must write an interger number.")
     else:
         return int(i)
+    
+def checkGreatestCommonDivisor(a, b):
+    if b == 0:
+        return a
+    else:
+        return checkGreatestCommonDivisor(b, (a % b))
 
 p = input("Write the first number: ")
 p = checkIsDigit(p)
 
 q = input("Write the second number: ")
 q = checkIsDigit(q)
+
+r = checkGreatestCommonDivisor(p, q)
+
+print("The GCD between {} and {} is: {}!".format(p, q, r))
